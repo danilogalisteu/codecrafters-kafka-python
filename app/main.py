177a72830258250e60fcb9fefb20163a56cda1c2
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-24 15:49:18 -0300

    parsing all header

--- app/main.py
+++ app/main.py
@@ -12,17 +12,18 @@ async def client_cb(client, addr):
     logging.warning(f"[{addr}] New connection")
 
     recv_message = b""
-    header_size = struct.calcsize(">ii")
+    header_format = ">ihhi"
+    header_size = struct.calcsize(header_format)
     while len(recv_message) < header_size:
         await curio.sleep(0)
         recv_message += await client.recv(100)
         logging.warning("[%s] Received %d %s", addr, len(recv_message), recv_message)
 
-    message_size, correlation_id = struct.unpack(">ii", recv_message[:header_size])
-    logging.warning("%d %d", message_size, correlation_id)
+    message_size, request_api_key, request_api_version, correlation_id = struct.unpack(header_format, recv_message[:header_size])
+    logging.warning("Header: %d %d %d %d", message_size, request_api_key, request_api_version, correlation_id)
     recv_message = recv_message[header_size:]
 
-    send_message = struct.pack(">ii", 0, 7)
+    send_message = struct.pack(">ii", 0, correlation_id)
     logging.warning("[%s] Sending %s", addr, send_message)
     await client.sendall(send_message)
 
