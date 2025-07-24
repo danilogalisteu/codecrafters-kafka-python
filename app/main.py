94dfd7544158aabd3b902ddf7f15c94a4886fc42
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-24 15:43:44 -0300

    server using curio

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
-import socket  # noqa: F401
+import logging
+import struct
+
+import curio
+
+logging.basicConfig(
+    format="[%(asctime)s|%(levelname)s] %(message)s", level=logging.INFO
+)
+
+
+async def client_cb(client, addr):
+    logging.warning(f"[{addr}] New connection")
 
 
 def main():
-    print("Logs from your program will appear here!")
+    logging.warning("Logs from your program will appear here!")
 
-    server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
+    curio.run(curio.tcp_server, "localhost", 9092, client_cb)
 
 
 if __name__ == "__main__":
