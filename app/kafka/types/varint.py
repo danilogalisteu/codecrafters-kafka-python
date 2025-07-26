f0103922832cb2e74a8955f0dfcd291a4a608246
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-26 19:07:22 -0300

    added signed values

--- app/kafka/types/varint.py
+++ app/kafka/types/varint.py
@@ -1,16 +1,19 @@
 import struct
 
 
-def decode_varint(recv_message: bytes) -> tuple[int, int]:
+def decode_varint(recv_message: bytes, signed: bool = False) -> tuple[int, int]:
     total = 0
     for i, value in enumerate(recv_message):
         total += (value & 0b01111111) << (i * 7)
         if value >> 7 == 0:
-            return i + 1, total
+            return i + 1, total if not signed else total >> 1 if total % 2 == 0 else -(
+                (total + 1) >> 1
+            )
     return 0, 0
 
 
-def encode_varint(total: int) -> bytes:
+def encode_varint(total: int, signed: bool = False) -> bytes:
+    total = total if not signed else total << 1 if total >= 0 else 2 * (-total) - 1
     data = b""
     while True:
         value = total & 0b01111111
