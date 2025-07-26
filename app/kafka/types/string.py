84e8c7d70bab6165a8100385215ae2917165f80f
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-25 21:15:16 -0300

    reordered functions

--- app/kafka/types/string.py
+++ app/kafka/types/string.py
@@ -3,15 +3,6 @@ import struct
 from .varint import decode_varint
 
 
-def decode_string_compact(recv_message):
-    pos, length = decode_varint(recv_message)
-    if pos == 0:
-        return 0, b""
-    if len(recv_message[pos:]) < length - 1:
-        return 0, b""
-    return length, recv_message[pos : pos + length - 1].decode()
-
-
 def decode_string(recv_message):
     if len(recv_message) < 2:
         return 0, b""
@@ -19,3 +10,12 @@ def decode_string(recv_message):
     if len(recv_message) < 2 + length:
         return 0, b""
     return 2 + length, recv_message[2 : 2 + length].decode()
+
+
+def decode_string_compact(recv_message):
+    pos, length = decode_varint(recv_message)
+    if pos == 0:
+        return 0, b""
+    if len(recv_message[pos:]) < length - 1:
+        return 0, b""
+    return length, recv_message[pos : pos + length - 1].decode()
