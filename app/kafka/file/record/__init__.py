76131de28fe261a9702e14500863c44a0c51d3ff
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-27 12:29:16 -0300

    added decoding for record

---
+++ app/kafka/file/record/__init__.py
@@ -0,0 +1,3 @@
+from .record import decode_record
+
+__all__ = ["decode_record"]
---
+++ app/kafka/file/record/record.py
@@ -0,0 +1,93 @@
+import logging
+
+from app.kafka.types.varint import decode_varint
+
+from .value import decode_record_value
+
+
+def decode_record(
+    buffer: bytes,
+) -> tuple[
+    int,
+    int,
+    int,
+    int,
+    int,
+    bytes,
+    int,
+    int,
+    int,
+    dict[str, str | int | bytes | list[str]],
+    int,
+    list[str],
+]:
+    logging.info("pre record %s", buffer[:40].hex(" "))
+
+    pos_length, length = decode_varint(buffer, signed=True)
+    if pos_length == 0:
+        return 0, 0, 0, 0, 0, b"", 0, 0, 0, {}, 0, []
+    buffer = buffer[pos_length:]
+    total_length = pos_length
+
+    if len(buffer) < 1:
+        return 0, 0, 0, 0, 0, b"", 0, 0, 0, {}, 0, []
+    attr = buffer[0]
+    buffer = buffer[1:]
+    total_length += 1
+
+    pos_ts, ts_delta = decode_varint(buffer, signed=True)
+    if pos_ts == 0:
+        return 0, 0, 0, 0, 0, b"", 0, 0, 0, {}, 0, []
+    buffer = buffer[pos_ts:]
+    total_length += pos_ts
+
+    pos_offset, offset_delta = decode_varint(buffer, signed=True)
+    if pos_offset == 0:
+        return 0, 0, 0, 0, 0, b"", 0, 0, 0, {}, 0, []
+    buffer = buffer[pos_offset:]
+    total_length += pos_offset
+
+    pos_key_length, key_length = decode_varint(buffer, signed=True)
+    if pos_key_length == 0:
+        return 0, 0, 0, 0, 0, b"", 0, 0, 0, {}, 0, []
+    buffer = buffer[pos_key_length:]
+    total_length += pos_key_length
+
+    if len(buffer) < key_length - 1:
+        return 0, 0, 0, 0, 0, b"", 0, 0, 0, {}, 0, []
+    key = b""
+    if key_length > -1:
+        pass
+
+    pos_value, frame_version, record_type, record_version, record_data = (
+        decode_record_value(buffer)
+    )
+    if pos_value == 0:
+        return 0, 0, 0, 0, 0, b"", 0, 0, 0, {}, 0, []
+    buffer = buffer[pos_value:]
+    total_length += pos_value
+
+    pos_headers, headers_count = decode_varint(buffer)
+    if pos_headers == 0:
+        return 0, 0, 0, 0, 0, b"", 0, 0, 0, {}, 0, []
+    buffer = buffer[pos_headers:]
+    total_length += pos_headers
+
+    headers: list[str] = []
+    for _ in range(headers_count):
+        pass
+
+    return (
+        total_length,
+        length,
+        attr,
+        ts_delta,
+        offset_delta,
+        key,
+        frame_version,
+        record_type,
+        record_version,
+        record_data,
+        headers_count,
+        headers,
+    )
