53639a51bb2c1cbb06c0393804ba6ef9ba755407
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-27 12:28:02 -0300

    added decodig ffor record value of different types, and handling unknown types

---
+++ app/kafka/file/record/value.py
@@ -0,0 +1,90 @@
+import logging
+import struct
+
+from app.kafka.types.varint import decode_varint
+
+from .feature_level import decode_record_feature_level
+from .partition import decode_record_partition
+from .topic import decode_record_topic
+
+
+def decode_record_value(
+    buffer: bytes,
+) -> tuple[int, int, int, int, dict[str, str | int | bytes | list[str]]]:
+    pos_value_length, value_length = decode_varint(buffer, signed=True)
+    if pos_value_length == 0:
+        return 0, 0, 0, 0, {}
+    buffer = buffer[pos_value_length:]
+    total_length = pos_value_length
+
+    if len(buffer) < value_length:
+        return 0, 0, 0, 0, {}
+
+    frame_version, record_type, record_version = struct.unpack(">BBB", buffer[:3])
+    buffer = buffer[3:]
+    total_length += 3
+
+    record_data: dict[str, str | int | bytes | list[str]] = {}
+    if record_type == 2:
+        pos_topic, topic_name, topic_uuid, fields_count, fields = decode_record_topic(
+            buffer
+        )
+        buffer = buffer[pos_topic:]
+        total_length += pos_topic
+        record_data = {
+            "topic_name": topic_name,
+            "topic_uuid": topic_uuid,
+            "fields": fields,
+        }
+    elif record_type == 3:
+        (
+            pos_partition,
+            partition_id,
+            topic_uuid,
+            replicas,
+            insync,
+            removing,
+            adding,
+            leader,
+            leader_epoch,
+            partition_epoch,
+            directories,
+            fields,
+        ) = decode_record_partition(buffer)
+        buffer = buffer[pos_partition:]
+        total_length += pos_partition
+        record_data = {
+            "partition": partition_id,
+            "topic_uuid": topic_uuid,
+            "leader": leader,
+            "leader_epoch": leader_epoch,
+            "partition_epoch": partition_epoch,
+            "replicas": replicas,
+            "insync": insync,
+            "removing": removing,
+            "adding": adding,
+            "directories": directories,
+            "fields": fields,
+        }
+    elif record_type == 12:
+        pos_feature_level, name, feature_level, fields_count, fields = (
+            decode_record_feature_level(buffer)
+        )
+        buffer = buffer[pos_feature_level:]
+        total_length += pos_feature_level
+        record_data = {
+            "name": name,
+            "feature_level": feature_level,
+            "fields": fields,
+        }
+    else:
+        logging.warning("unhandled record type %d", record_type)
+        total_length += value_length - 3
+
+    return (
+        total_length,
+        frame_version,
+        record_type,
+        record_version,
+        record_data,
+    )
