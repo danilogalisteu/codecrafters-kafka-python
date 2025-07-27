557491a7dd8b18d3ff6358d7af372c12f66bdac1
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-27 11:11:48 -0300

    added decode_header

---
+++ app/kafka/file/header.py
@@ -0,0 +1,11 @@
+import logging
+import struct
+
+
+def decode_header(buffer: bytes) -> tuple[int, int, int, int, int, int]:
+    logging.info("pre header %s", buffer[:25].hex(" "))
+    header_format = ">QIIBI"
+    header_length = struct.calcsize(header_format)
+    if len(buffer) < header_length:
+        return 0, 0, 0, 0, 0, 0
+    return header_length, *struct.unpack(header_format, buffer[:header_length])
