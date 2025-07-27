4302bb667eda6524896e40eb596ed1dfbe86fd95
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-27 11:29:02 -0300

    added decoding for topic record

---
+++ app/kafka/file/record/topic.py
@@ -0,0 +1,22 @@
+from app.kafka.types.string import decode_string_compact
+from app.kafka.types.varint import decode_varint
+
+
+def decode_record_topic(buffer: bytes) -> tuple[int, str, bytes, int, list[str]]:
+    pos_name, name = decode_string_compact(buffer)
+    buffer = buffer[pos_name:]
+    total_length = pos_name
+
+    topic_uuid = buffer[:16]
+    buffer = buffer[16:]
+    total_length += 16
+
+    pos_fields, fields_count = decode_varint(buffer)
+    buffer = buffer[pos_fields:]
+    total_length += pos_fields
+
+    fields: list[str] = []
+    for _ in range(fields_count):
+        pass
+
+    return total_length, name, topic_uuid, fields_count, fields
