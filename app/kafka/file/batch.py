cba613d72b10580631070a46a78351f08a1d42a9
Danilo Galisteu <danilogalisteu@gmail.com>
2025-07-27 12:29:57 -0300

    added decoding for single batch

---
+++ app/kafka/file/batch.py
@@ -0,0 +1,97 @@
+import logging
+
+from .header import decode_header
+from .info import decode_info
+from .record import decode_record
+
+
+def decode_batch(buffer: bytes) -> tuple[int, dict[str, int]]:
+    pos_header, record_batch, batch_length, epoch, version, crc = decode_header(buffer)
+    if pos_header == 0:
+        return 0, {}
+    buffer = buffer[pos_header:]
+    logging.info(
+        "Meta Header: %d %d %d %d %d %s",
+        pos_header,
+        record_batch,
+        batch_length,
+        epoch,
+        version,
+        crc.to_bytes(4).hex(" "),
+    )
+
+    pos_info, attr, offset, ts_base, ts_max, pid, pepoch, seq, records_length = (
+        decode_info(buffer)
+    )
+    if pos_info == 0:
+        return 0, {}
+    buffer = buffer[pos_info:]
+    logging.info(
+        "Meta Info: %d %d %d %d %d %d %d %d %d",
+        pos_info,
+        attr,
+        offset,
+        ts_base,
+        ts_max,
+        pid,
+        pepoch,
+        seq,
+        records_length,
+    )
+
+    records = []
+    pos_records = 0
+    for _ in range(records_length):
+        (
+            pos_record,
+            length,
+            attr,
+            ts_delta,
+            offset_delta,
+            key,
+            frame_version,
+            record_type,
+            record_version,
+            record_data,
+            headers_count,
+            headers,
+        ) = decode_record(buffer)
+        if pos_record == 0:
+            return {}
+        logging.info(
+            "Meta Record: %d %d %d %d %d %s %d %d %d %s %d %s",
+            pos_record,
+            length,
+            attr,
+            ts_delta,
+            offset_delta,
+            key,
+            frame_version,
+            record_type,
+            record_version,
+            record_data,
+            headers_count,
+            headers,
+        )
+        buffer = buffer[pos_record:]
+        records.append(
+            (
+                length,
+                attr,
+                ts_delta,
+                offset_delta,
+                key,
+                frame_version,
+                record_type,
+                record_version,
+                record_data,
+                headers,
+            )
+        )
+        pos_records += pos_record
+
+    return pos_header + pos_info + pos_records, {
+        "header": (record_batch, batch_length, epoch, version, crc),
+        "info": (attr, offset, ts_base, ts_max, pid, pepoch, seq, records_length),
+        "records": records,
+    }
